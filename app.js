1c0639143ae65b7f219ee96798a94de8b0d0b420
Destynee Martine <deermartinez8@gmail.com>
2020-12-10 17:37:21 -0700

    arrays

---
+++ app.js
@@ -0,0 +1,27 @@
+
+//data structure than ca store and organize values in numbered/index list
+//store booleans, numbers, arrays (or nested arrays)
+
+//Array literal [] stored in a variable
+var numbers = [9, 10, 11, 12];
+console.log(number);
+
+//creating an array with a new js keyword
+
+var strings = new Array("lime" ,"cherry", "lemon", "orange");
+console.log(strings);
+
+//use js new keyword to matk the index
+
+Boolean[0] = 9<10;
+Boolean[1] = 11===11.5
+Boolean[2] = 8>12;
+console.log("boolean");
+
+//creat an arry with all the students
+//use one of the methods to creat an array
+
+var students = new Array("Destynee", "Marvin", "Abraham", "Kevin", "Ngoc", "Sheily", "Syed");
+console.log(students);
+
+
