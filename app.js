36a516002901cc54a7e9ef8f7af3af2683a276a7
Destynee Martine <deermartinez8@gmail.com>
2020-12-10 18:18:30 -0700

    array methods

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 //Array literal [] stored in a variable
 var numbers = [9, 10, 11, 12];
-console.log(number);
+console.log(numbers);
 
 //creating an array with a new js keyword
 
@@ -21,7 +21,7 @@ console.log("boolean");
 //creat an arry with all the students
 //use one of the methods to creat an array
 
-var students = new Array()
+var classList = new Array()
 classList[0] = "Destynee";
 classList[1] = "Marvin";
 classList[2] = "Abraham";
@@ -39,5 +39,47 @@ console.log(classList[6])
 
 //,length method will give index number of array
 console.log(classList.length);
+
+//array methods
+//built-in methods .methodname(data);
+// method is .push 
+classList.push("Krystina");
+// .push adds an item to the end of the array
+//.pop
+classList.pop();
+//.pop removes the last item on the list
+
+//.shift
+classList.shift();
+//.shift shifts everyone down an index
+// removes index from the beginning
+
+
+//.unshift
+classList.unshift("Destynee");
+//,unshift adds a new index to beginning of array
+
+
+
+//looping over an array
+//to see all items in an array iterations
+//for loop - most common
+//while loop
+//forEach loop
+
+//3 things we need to loop through data
+for (i = 0; i<classList.link; i++) {
+//It is starting at [0]
+// and will not stop until it is at the full length of the class
+//how it is moving-incraments or not(++)
+console.log(classList[i]);
+}
+
+
+
+
+
+
+
 
 
