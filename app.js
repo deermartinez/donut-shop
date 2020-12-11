e109bc18c8076026fd8db0120399b676593af94e
Destynee Martine <deermartinez8@gmail.com>
2020-12-10 20:52:34 -0700

    exercises

--- app.js
+++ app.js
@@ -43,8 +43,9 @@ console.log(classList.length);
 //array methods
 //built-in methods .methodname(data);
 // method is .push 
-classList.push("Krystina");
+classList.push("Krys");
 // .push adds an item to the end of the array
+
 //.pop
 classList.pop();
 //.pop removes the last item on the list
@@ -58,6 +59,13 @@ classList.shift();
 //.unshift
 classList.unshift("Destynee");
 //,unshift adds a new index to beginning of array
+//.unshift replace 1 element at a certain index
+
+//.splice
+classList.splice(3, 0, "Jeep");
+//.splice (index number, number of changes, replacement)
+//.splice(1,0=no changes just add on =<1, replaces)
+
 
 
 
@@ -74,6 +82,51 @@ for (i = 0; i<classList.link; i++) {
 //how it is moving-incraments or not(++)
 console.log(classList[i]);
 }
+
+
+//.indexOf()
+console.log(classList.indexOf("Syed"));
+//.indexOf finds a specific array item from list
+
+
+
+var donuts = new Array()
+donuts[0] = "Glazed";
+donuts[1] = "Chocolate";
+donuts[2] = "Bites";
+donuts[3] = "Squares";
+donuts[4] = "Plain";
+donuts[5] = "Jelly";
+donuts[6] = "Croissant";
+donuts[7] = "Sugar";
+
+
+
+console.log(donuts);
+
+
+
+for (i = 0; i<donuts.link; i++) {
+    console.log(donuts[i]);
+    }
+
+
+donuts.shift();
+donuts.pop();
+donuts.unshift("Lime Zest");
+donuts.unshift("Lemon Pie");
+
+var moreDonuts = ["Glazed", "Chocolate", "Bites", "Squares", "Jelly","Bagel", "Croissant", "Sugar"];
+document.getElementById("Home").innerHTML = moreDonuts.join(" ");
+
+var trash = donuts.slice(0); 
+
+console.log(donuts.indexOf("Chocolate"));
+console.log(donuts.indexOf("Glazed"));
+
+donuts.splice(7, 1, "Cherry");
+
+var withSyed = [classList + "Bob"];
 
 
 
